d3bad64cb82b09d38ef2146dcdfaefc02457bc6d
MAROM-PC\marom <marom.tech7@gmail.com>
2023-12-20 09:58:59 +0200

    Added auth middleware and API calling to uers

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -60,5 +60,18 @@ const authUser = asyncHandler(async (req, res) => {
     throw new Error("Invalid Email or Password! ");
   }
 });
-
-module.exports = { registerUser, authUser };
+// Querys
+const allUsers = asyncHandler(async (req, res) => {
+  const keyword = req.query.search
+    ? {
+        $or: [
+          { name: { $regex: req.query.search, $options: "i" } },
+          { email: { $regex: req.query.search, $options: "i" } },
+        ],
+      }
+    : {};
+  // Exclude the logged-in user
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+module.exports = { registerUser, authUser, allUsers };
